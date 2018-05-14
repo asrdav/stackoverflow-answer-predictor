52f524ef5ba3907406483b82ec09ad0a763dc526
Sid <abhinavsiddharth8409@gmail.com>
2018-05-14 12:50:00 +0530

    Create feature vectors

--- Feature_vectors_creation.py
+++ Feature_vectors_creation.py
@@ -1,6 +1,6 @@
-import readability
-import information
-import text_properties
+import readability #importing necessary libraries
+import info
+import properties
 
 
 class featureVectors:
@@ -8,7 +8,7 @@ class featureVectors:
     def assignValue(myList):
         list2 = sorted(myList, reverse=True)
         values = []
-        const = 5040
+        const = 5040 #normalization factor
         n = len(myList)
         for value1 in myList:
             for i in range(0, n):
@@ -17,8 +17,8 @@ class featureVectors:
                     break
         return values
 
-    def getInformation(self, listOfDicts):
-        inf = information.Informativity()
+    def getInformation(self, listOfDicts): #getting information provided by the answer body using markov model and entropy
+        inf = info.Informativity()
         listOfInformation = []
         for row in listOfDicts:
             totalEntropy = 0
@@ -28,127 +28,127 @@ class featureVectors:
             listOfInformation.append(abs(totalEntropy))
         return self.assignValue(listOfInformation)
 
-    def getRelevancy(self, listOfDicts):
-        tp = text_properties.TextProperties()
+    def getRelevancy(self, listOfDicts): #getting relevancy between answerBody, qBody, qTags
+        tp = properties.TextProperties()
         listOfRel = []
         for row in listOfDicts:
             listOfRel.append(tp.relevancy(row['answerBody'], row['qBody'], row['qTags']))
         return self.assignValue(listOfRel)
 
-    def getUniqueWords(self, listOfDicts):
-        tp = text_properties.TextProperties()
+    def getUniqueWords(self, listOfDicts): #getting unique words in answerBody
+        tp = properties.TextProperties()
         listOfUw = []
         for row in listOfDicts:
             listOfUw.append(tp.UniqueWords(row['answerBody']))
         return self.assignValue(listOfUw)
 
-    def getNSWords(self, listOfDicts):
-        tp = text_properties.TextProperties()
+    def getNSWords(self, listOfDicts): #getting Nonstop-Words in answerBody
+        tp = properties.TextProperties()
         listOfNs = []
         for row in listOfDicts:
             listOfNs.append(tp.NonstopWords(row['answerBody']))
         return self.assignValue(listOfNs)
 
-    def getSub(self, listOfDicts):
-        tp = text_properties.TextProperties()
+    def getSub(self, listOfDicts): #getting subjectivity of answerBody
+        tp = properties.TextProperties()
         listOfSub = []
         for row in listOfDicts:
             listOfSub.append(tp.subjective(row['answerBody']))
         return self.assignValue(listOfSub)
 
-    def getAS(self, listOfDicts):
+    def getAS(self, listOfDicts): #using answerScore as feature
         listOfAs = []
         for row in listOfDicts:
             listOfAs.append(row['answerScore'])
         return self.assignValue(listOfAs)
 
-    def getDV(self, listOfDicts):
+    def getDV(self, listOfDicts): #using negation of number of answererDownvotes as a feature
         listOfDV = []
         for row in listOfDicts:
             listOfDV.append(row['answererDownvotes'] * -1)
         return self.assignValue(listOfDV)
 
-    def getUV(self, listOfDicts):
+    def getUV(self, listOfDicts): #using number of answererUpvotes as a feature
         listOfUV = []
         for row in listOfDicts:
             listOfUV.append(row['answererUpvotes'])
         return self.assignValue(listOfUV)
 
-    def getComm(self, listOfDicts):
+    def getComm(self, listOfDicts): #using number of comments on the answer as a feature
         listOfComm = []
         for row in listOfDicts:
             listOfComm.append(row['answerCommCount'])
         return self.assignValue(listOfComm)
 
-    def getRead(self, listOfDicts):
+    def getRead(self, listOfDicts): #using readabilty index of answerBody as a feature
         listOfRead = []
         ts = readability.TextStatistics()
         for row in listOfDicts:
             listOfRead.append(ts.text_standard(row['answerBody']))
         return self.assignValue(listOfRead)
 
-    def getDW(self, listOfDicts):
+    def getDW(self, listOfDicts): #using difficult words in answerBody as a feature
         listOfDW = []
         ts = readability.TextStatistics()
         for row in listOfDicts:
             listOfDW.append(ts.difficult_words(row['answerBody']))
         return self.assignValue(listOfDW)
 
-    def getRepu(self, listOfDicts):
+    def getRepu(self, listOfDicts): #using answerer reputation as a feature 
         listOfRepu = []
         for row in listOfDicts:
             listOfRepu.append(row['answererReputation'])
         return self.assignValue(listOfRepu)
 
-    def cfv(self, listOfDicts):
+    def cfv(self, listOfDicts): #to create feature vector
         n = len(listOfDicts)
         features = [[] for _ in range(n)]
 
-        infor = self.getInformation(listOfDicts)
+        infor = self.getInformation(listOfDicts) #1
         for i in range(0, n):
             features[i].append(infor[i])
 
-        rel = self.getRelevancy(listOfDicts)
+        rel = self.getRelevancy(listOfDicts) #2
         for i in range(0, n):
             features[i].append(rel[i])
 
-        uw = self.getUniqueWords(listOfDicts)
+        uw = self.getUniqueWords(listOfDicts) #3
         for i in range(0, n):
             features[i].append(uw[i])
 
-        ns = self.getNSWords(listOfDicts)
+        ns = self.getNSWords(listOfDicts) #4
         for i in range(0, n):
             features[i].append(ns[i])
 
-        sub = self.getSub(listOfDicts)
+        sub = self.getSub(listOfDicts) #5
         for i in range(0, n):
             features[i].append(sub[i])
 
-        answerScore = self.getAS(listOfDicts)
+        answerScore = self.getAS(listOfDicts) #6
         for i in range(0, n):
             features[i].append(answerScore[i])
 
-        downVotes = self.getDV(listOfDicts)
+        downVotes = self.getDV(listOfDicts) #7
         for i in range(0, n):
             features[i].append(downVotes[i])
 
-        upVotes = self.getUV(listOfDicts)
+        upVotes = self.getUV(listOfDicts) #8
         for i in range(0, n):
             features[i].append(upVotes[i])
 
-        comments = self.getComm(listOfDicts)
+        comments = self.getComm(listOfDicts) #9
         for i in range(0, n):
             features[i].append(comments[i])
 
-        readable = self.getRead(listOfDicts)
+        readable = self.getRead(listOfDicts) #10
         for i in range(0, n):
             features[i].append(readable[i])
 
-        diffWords = self.getDW(listOfDicts)
+        diffWords = self.getDW(listOfDicts) #11
         for i in range(0, n):
             features[i].append(diffWords[i])
 
-        repu = self.getRepu(listOfDicts)
+        repu = self.getRepu(listOfDicts) #12
         for i in range(0, n):
             features[i].append(repu[i])
 
